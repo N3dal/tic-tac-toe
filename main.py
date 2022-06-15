a3a4b6a71b06bf2eae32abec9d7a392f22090383
N3dal <nedalxzo@gmail.com>
2022-06-15 16:32:04 +0200

    add the possible moves to win as const-var.

--- main.py
+++ main.py
@@ -21,6 +21,26 @@ from random import randint
 # link for lines that i used for drawing the game map:
 # https: // en.wikipedia.org/wiki/Box-drawing_character
 
+
+# possible moves for user or python to win.
+MOVES_TO_WIN = [
+    # horizontal moves to win.
+    ([0, 0], [0, 1], [0, 2]),
+    ([1, 0], [1, 1], [1, 2]),
+    ([2, 0], [2, 1], [2, 2]),
+
+    # vertical moves to win.
+    ([0, 0], [1, 0], [2, 0]),
+    ([0, 1], [1, 1], [2, 1]),
+    ([0, 2], [1, 2], [2, 2]),
+
+    # diagonal moves to win.
+    ([0, 0], [1, 1], [2, 2]),
+    ([0, 2], [1, 1], [2, 0])
+
+]
+
+
 def clear():
     """wipe the terminal."""
 
